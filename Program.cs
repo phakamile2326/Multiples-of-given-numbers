1f54e9f01054620a0a6a6e83bdb72f21ea4cbb6e
Dell-User <Dell-User@DESKTOP-M748B2N>
2023-10-17 05:38:54 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples_of_given_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nEnter first integer: ");
+            int x= Convert.ToInt32(Console.ReadLine());
+            if (x>0)
+            {
+                Console.WriteLine(x % 3 == 0 || x % 7 == 0);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
